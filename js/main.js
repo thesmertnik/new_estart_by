056085da32d8a0f46f79d231a2211e7facdaab17
thesmertnik <eyeofwhitenight@yandex.ru>
2023-03-10 17:43:26 +0300

    0.1.8 добавлены ключевые слова

--- js/main.js
+++ js/main.js
@@ -4,3 +4,14 @@ $(document).ready(function (){
   })
 })
 
+function CookiesDelete() {
+  let cookies = document.cookie.split(";");
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i];
+    let eqPos = cookie.indexOf("=");
+    let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;";
+    document.cookie = name + '=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  }
+}
+
